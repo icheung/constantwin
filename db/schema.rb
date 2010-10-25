72643b705fb4b54feff9118b972b49756c4966ee
q10 <q101100111000@gmail.com>
2010-10-24 22:51:26 -0700

    false? removed from task.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101027005740) do
+ActiveRecord::Schema.define(:version => 20101027014927) do
 
   create_table "tasks", :force => true do |t|
     t.string   "description"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(:version => 20101027005740) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.time     "started_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
