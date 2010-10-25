4ac85fb0032f087b05eb5dea5745a49a1ec8e5a8
q10 <q101100111000@gmail.com>
2010-10-24 22:24:51 -0700

    more task specs

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,16 @@ class Task < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :duration
   validates_presence_of :owner
+  validates_presence_of :is_finished, :if => "is_finished.false?"
+  validates_length_of :description, :allow_blank => false, :allow_nil => false, :minimum => 5, :too_short => "Task Description must be at least 5 characters long!"
 
   # assoc, can uncomment later
   # belongs_to :user
+
+  # ensures is_finished is false by default, not nil
+  def initialize(params=nil)
+    super
+    self.is_finished = false
+    self.started_at = nil
+  end
 end
