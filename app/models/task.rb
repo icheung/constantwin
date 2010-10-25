7cdb16ccd5bbd65b61ae80cfb32683bbae8641be
q10 <q101100111000@gmail.com>
2010-10-24 22:54:20 -0700

    is_finished fixed for task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,6 @@ class Task < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :duration
   validates_presence_of :owner
-  validates_presence_of :is_finished, :if => "is_finished == false"
   validates_length_of :description, :allow_blank => false, :allow_nil => false, :minimum => 5, :too_short => "Task Description must be at least 5 characters long!"
 
   belongs_to :user
