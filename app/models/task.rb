2efbc96e95d8ceec15afaf3ba17f5cbaf49217bc
Bryan Tsang <bvtsang@gmail.com>
2010-10-26 19:20:49 -0700

    Removed owner field from /tasks/new

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,14 +2,14 @@ class Task < ActiveRecord::Base
   validates_presence_of :description
   validates_presence_of :duration
   validates_presence_of :owner
-  validates_presence_of :is_finished, :if => "is_finished == false?"
-  validates_length_of :description, :allow_blank => false, :allow_nil => false, :minimum => 5, :too_short => "Task Description must be at least 5 characters long!"
+  validates_length_of :description, :allow_blank => false, :allow_nil => false, :minimum => 5, :too_short => "must be at least 5 characters long!"
 
   belongs_to :user
 
   # ensures is_finished is false by default, not nil
   def initialize(params=nil)
     super
+    self.owner = 0  # For now.  Later, we keep track of users.
     self.is_finished = false
     self.started_at = nil
   end
