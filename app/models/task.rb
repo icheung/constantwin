2563ad28cf2186d1d190f874c2a5b36ba6948a24
Bryan Tsang <bvtsang@gmail.com>
2010-10-31 14:49:55 -0700

    Added before_validation :default_values to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,17 +5,20 @@ class Task < ActiveRecord::Base
   validates_presence_of :is_finished, :if => "#{:is_finished}"
   validates_length_of :description, :allow_blank => false, :allow_nil => false, :minimum => 5, :too_short => "Task Description must be at least 5 characters long!"
   validate :new_task_cannot_have_no_owner, :duration_cannot_be_less_than_5_minutes, :new_task_cannot_be_already_started, :new_task_cannot_be_already_finished
-
   belongs_to :user
+  before_validation :default_values
 
   # ensures is_finished is false by default, not nil
   def initialize(params=nil)
     super
-    self.owner = 0 # For now.  Later, we keep track of users. PERHAPS we don't need this b/c one can call @task.user_id using Rails Associations - COMMENT THIS LINE OUT FOR A SPEC TEST TO WORK
-    self.is_finished = false unless self.is_finished
-    self.started_at = nil unless self.started_at
   end
 
+  def default_values
+    self.owner = 0 unless self.owner # For now.  Later, we keep track of users. PERHAPS we don't need this b/c one can call @task.user_id using Rails Associations - COMMENT THIS LINE OUT FOR A SPEC TEST TO WORK
+    self.is_finished = false unless self.is_finished
+    self.started_at = nil unless self.started_at
+    self.duration = Time.now unless self.duration
+  end
   
   # special validations checks
 
