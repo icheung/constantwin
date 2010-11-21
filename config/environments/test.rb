4fe1a473d2eb2aa1d36c15dd1f9e3aa370c1c0b0
Bryan Tsang <bvtsang@gmail.com>
2010-11-21 13:28:49 -0800

    Oops, forgot to git add changes to factory_girl

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,4 +28,4 @@ config.action_mailer.delivery_method = :test
 # config.active_record.schema_format = :sql
 
   config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
-  config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
+  config.gem 'factory_girl', :lib => 'factory_girl', :source => 'http://gemcutter.org'
