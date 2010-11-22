9559654bad28fddce1871d5a5ee2754e657075fd
Matthew DuVall <mduvall89@gmail.com>
2010-11-21 23:02:07 -0800

    more fine grained progressbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ActiveRecord::Base
 
   #find the user in the database, first by the facebook user id and if that fails through the email hash
   def self.find_by_fb_user(fb_user)
-    User.find_by_fb_user_id(fb_user.uid) || User.find_by_email_hash(fb_user.email_hashes)
+#    User.find_by_fb_user_id(fb_user.uid) || User.find_by_email_hash(fb_user.email_hashes)
   end
   
   #Take the data returned from facebook and create a new user from it.
